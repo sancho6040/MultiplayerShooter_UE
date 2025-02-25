d53fd2940d307c3604bd9fd4b28a034b8335a334
sancho6040 <qwetito@gmail.com>
2025-02-24 20:47:31 -0500

    plugin configuration

--- Plugins/MultiplayerSessionsPlugin/Source/MultiplayerSessionsPlugin/MultiplayerSessionsPlugin.Build.cs
+++ Plugins/MultiplayerSessionsPlugin/Source/MultiplayerSessionsPlugin/MultiplayerSessionsPlugin.Build.cs
@@ -27,6 +27,8 @@ public class MultiplayerSessionsPlugin : ModuleRules
 			{
 				"Core",
 				// ... add other public dependencies that you statically link with here ...
+				"OnlineSubsystem",
+				"OnlineSubsystemSteam"
 			}
 			);
 			
